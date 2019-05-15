0f55f81c466ed2f33bb4efad6dab42b62d888f87
altmanWang <qx_wang@foxmail.com>
2019-05-15 10:05:08 +0800

    404. Sum of Left Leaves

---
+++ 404. Sum of Left Leaves/src/Solution.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using std::vector;
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(nullptr == root)
+            return 0;
+        if(nullptr != root->left && nullptr == root->left->left && nullptr == root->left->right)
+            return root->left->val + sumOfLeftLeaves(root->right);
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+
+int main(){
+    
+    TreeNode root(3);
+    root.left = new TreeNode(9);
+    root.right = new TreeNode(20);
+    root.right->left = new TreeNode(15);
+    root.right->right = new TreeNode(7);
+    Solution solution;
+    int res = solution.sumOfLeftLeaves(&root);
+    std::cout << res << std::endl;
+
+    return 0;
+}
